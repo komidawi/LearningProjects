577221316898e393c37a5ad60c028352d985334a
Dawid Komisarczuk <komidawi@gmail.com>
2024-01-16 18:37:07 +0100

    [Angular/ToH] List and select Heroes for details

--- FRONTEND/ANGULAR/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ FRONTEND/ANGULAR/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,14 +1,18 @@
-import {Component} from '@angular/core';
-import {Hero} from "../hero";
+import { Component } from '@angular/core'
+import { HEROES } from '../mock-heroes'
+import { Hero } from '../hero'
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
+  heroes = HEROES
+
+  selectedHero?: Hero
+
+  onSelect (hero: Hero) {
+    this.selectedHero = hero
   }
 }
