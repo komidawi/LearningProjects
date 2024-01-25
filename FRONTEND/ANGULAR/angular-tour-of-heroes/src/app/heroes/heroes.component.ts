ae65f5bc67af42ea37c4309ff66827c1a8084987
Dawid Komisarczuk <dawid.komisarczuk@neontri.com>
2024-01-25 21:00:05 +0100

    [Angular/ToH] Log clicks on heroes in HeroesComponent

--- FRONTEND/ANGULAR/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ FRONTEND/ANGULAR/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
 import { Hero } from '../hero'
 import { HeroService } from '../hero.service'
+import { MessageService } from '../message.service'
 
 @Component({
   selector: 'app-heroes',
@@ -11,7 +12,7 @@ export class HeroesComponent {
   heroes: Hero[] = []
   selectedHero?: Hero
 
-  constructor (private heroService: HeroService) {}
+  constructor (private heroService: HeroService, private messageService: MessageService) {}
 
   ngOnInit () {
     this.setupHeroes()
@@ -24,5 +25,6 @@ export class HeroesComponent {
 
   onSelect (hero: Hero) {
     this.selectedHero = hero
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 }
