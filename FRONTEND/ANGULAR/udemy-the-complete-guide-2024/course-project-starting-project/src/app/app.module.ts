68378116e275b912367bdafb1492094253026aa1
Dawid Komisarczuk <komidawi@gmail.com>
2024-04-06 09:38:44 +0200

    [Angular/tcg2024] Initialize header, recipe, shopping-list Components

--- FRONTEND/ANGULAR/udemy-the-complete-guide-2024/course-project-starting-project/src/app/app.module.ts
+++ FRONTEND/ANGULAR/udemy-the-complete-guide-2024/course-project-starting-project/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
