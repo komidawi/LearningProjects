211de791198b38fc182013bd4c66aa79e113cae2
Dawid Komisarczuk <komidawi@gmail.com>
2024-04-07 09:40:27 +0200

    [Angular/tcg2024] Add Recipe

--- FRONTEND/ANGULAR/udemy-the-complete-guide-2024/course-project-starting-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ FRONTEND/ANGULAR/udemy-the-complete-guide-2024/course-project-starting-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Test Description', 'https://angular.io/assets/images/logos/angular/logo-nav@2x.png')
+  ]
 }
