633367a32cb27e77d82322481144f7c949b275a6
Dawid Komisarczuk <komidawi@gmail.com>
2024-04-08 14:55:12 +0200

    [Angular/tcg2024] CDP: Use @ViewChild to get data from input

--- FRONTEND/ANGULAR/udemy-the-complete-guide-2024/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ FRONTEND/ANGULAR/udemy-the-complete-guide-2024/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core'
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core'
 
 @Component({
   selector: 'app-cockpit',
@@ -9,19 +9,19 @@ export class CockpitComponent {
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>()
   @Output() blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>()
 
-  newServerContent = ''
+  @ViewChild('serverContentInput') newServerContent: ElementRef
 
   onAddServer (serverNameInput: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: serverNameInput.name,
-      serverContent: this.newServerContent
+      serverContent: this.newServerContent.nativeElement.value
     })
   }
 
   onAddBlueprint (serverNameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: serverNameInput.name,
-      serverContent: this.newServerContent
+      serverContent: this.newServerContent.nativeElement.value
     })
   }
 }
