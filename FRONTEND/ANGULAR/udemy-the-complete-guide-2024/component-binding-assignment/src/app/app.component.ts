781eacb98dda345349b1e94c620e015bb3f382ec
Dawid Komisarczuk <komidawi@gmail.com>
2024-04-08 18:22:53 +0200

    [Angular/tcg2024] Component Binding Assignment: Complete

--- FRONTEND/ANGULAR/udemy-the-complete-guide-2024/component-binding-assignment/src/app/app.component.ts
+++ FRONTEND/ANGULAR/udemy-the-complete-guide-2024/component-binding-assignment/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'component-binding-assignment';
+
+  oddNumbers: number[] = []
+  evenNumbers: number[] = []
+
+  onIntervalFired (counter: number) {
+    console.log('Number received: ' + counter)
+    if (counter % 2) {
+      this.evenNumbers.push(counter)
+    } else {
+      this.oddNumbers.push(counter)
+    }
+    console.log('Even Numbers ' + this.evenNumbers)
+    console.log('Odd Numbers ' + this.oddNumbers)
+  }
 }
