11bf86d68ffcfef878bc41ab3fea266f16f11877
Dawid Komisarczuk <komidawi@gmail.com>
2024-04-09 10:28:23 +0200

    [Angular/tcg2024] Display Recipe Details

--- FRONTEND/ANGULAR/udemy-the-complete-guide-2024/course-project-starting-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ FRONTEND/ANGULAR/udemy-the-complete-guide-2024/course-project-starting-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core'
 import { Recipe } from '../recipe.model'
 
 @Component({
@@ -7,7 +7,13 @@ import { Recipe } from '../recipe.model'
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  @Output() recipeSelected = new EventEmitter<Recipe>()
+
   recipes: Recipe[] = [
     new Recipe('Test Recipe', 'Test Description', 'https://angular.io/assets/images/logos/angular/logo-nav@2x.png')
   ]
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe)
+  }
 }
