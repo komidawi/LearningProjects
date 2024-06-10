c4ee0adef7b23102fda51f636d8050ff136b44c7
Dawid Komisarczuk <komidawi@gmail.com>
2024-06-10 12:24:04 +0200

    [Angular/tcg2024] Directives: Use Directive to listen to events

--- FRONTEND/ANGULAR/udemy-the-complete-guide-2024/directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ FRONTEND/ANGULAR/udemy-the-complete-guide-2024/directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,13 +1,17 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core'
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core'
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
-export class BetterHighlightDirective implements OnInit {
+export class BetterHighlightDirective {
 
   constructor (private elementRef: ElementRef, private renderer: Renderer2) { }
 
-  ngOnInit () {
+  @HostListener('mouseenter') mouseenter (event: Event) {
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue')
+  }
+
+  @HostListener('mouseleave') mouseleave (event: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent')
   }
 }
