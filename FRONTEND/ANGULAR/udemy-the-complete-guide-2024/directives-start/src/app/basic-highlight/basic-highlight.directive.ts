0456fba0b95f009538b01e35ac3a178e4b8f4005
Dawid Komisarczuk <komidawi@gmail.com>
2024-06-10 11:49:02 +0200

    [Angular/tcg2024] Directives: Add BetterHighlightDirective

--- FRONTEND/ANGULAR/udemy-the-complete-guide-2024/directives-start/src/app/basic-highlight/basic-highlight.directive.ts
+++ FRONTEND/ANGULAR/udemy-the-complete-guide-2024/directives-start/src/app/basic-highlight/basic-highlight.directive.ts
@@ -7,6 +7,10 @@ export class BasicHighlightDirective implements OnInit {
   constructor (private elementRef: ElementRef) {}
 
   ngOnInit () {
+    /* Don't do this way! Use renderer instead!
+    It is because not all environments have DOM,
+    so manually editing DOM just won't work in such cases.
+    Using Renderer2 is a good practice. */
     this.elementRef.nativeElement.style.backgroundColor = 'green'
   }
 }
