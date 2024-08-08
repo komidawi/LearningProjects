8da3b86a233250f2a86ece8f152c0eec7a06fee1
Dawid Komisarczuk <komidawi@gmail.com>
2024-08-08 15:04:27 +0200

    [Angular/tcg2024] Playground: Show separate binding concepts

--- FRONTEND/ANGULAR/udemy-the-complete-guide-2024/playground/src/app/app.module.ts
+++ FRONTEND/ANGULAR/udemy-the-complete-guide-2024/playground/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
+import { ChildComponentComponent } from './child-component/child-component.component'
+import { FormsModule } from '@angular/forms'
+import { LabelkaComponent } from './labelka/labelka.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponentComponent,
+    LabelkaComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
